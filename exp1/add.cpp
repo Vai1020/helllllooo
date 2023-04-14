411e6be2de57becca5e789e91b0f54b37a95366d
Vai1020 <102492742+Vai1020@users.noreply.github.com>
2023-04-14 19:02:45 +0530

    Add files via upload

---
+++ exp1/add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first_number, second_number, sum;
+  int a=4;
+  int b=6;
+  sum = a + b;
+
+  cout << a << " + " <<  b << " = " << sum;     
+
+  return 0;
+}
